c4291bae6b4d458751c50b8d10e0eaf38e98919e
noone <noone>
2013-12-17 19:54:05 -0700

    Made a better attractMode() function that plays a tune along with blinking lights.  Should be enter

--- Firmware/Simon_Sings/Simon_Sings.ino
+++ Firmware/Simon_Sings/Simon_Sings.ino
@@ -2,18 +2,41 @@
 /*
 This is a much hacked up version of the Simon-Says to make it into a
 more toddler friendly "Simon-Sings".  Pressing the buttons now plays a tone
-and lites up the LEDs, and nothing else.  Should amuse small children for
-some time.  Maybe even encourage them to dance to their own music.
+and lites up the LEDs, and nothing else.  May amuse small children for
+some time.  (You) Should encourage them to dance to 80's style 8 bit
+beeping (aka music).
 */
 
 #include "hardware_versions.h"
 #include "pitches.h"
 
-//How long should a keypress make noise?
+//How long should a keypress make noise and LED stay lit?
 #define TONE_LENGTH 10
+//how often should we blink when in attractMode
+#define BLINK_EVERY 250
+//wait these number of ms until calling attractMode
+#define TIME_TILL_BLINKY 60*1000
+
+/*
+Make this as long as you wish.  This melody is blatently taken from the
+Simon Says "cheat mode" from sfe. The length is retrieved by the sizeof()
+operator, so you dont need to remember how long this is until you have
+more characters than can fit in a sizeof(unsigned int) (which is 65536)
+*/
+int melody[] = {NOTE_G4, NOTE_A4,       0, NOTE_C5,
+                      0,       0, NOTE_G4,       0,
+                      0,       0, NOTE_E4,       0,
+                NOTE_D4, NOTE_E4, NOTE_G4,       0,
+                NOTE_D4, NOTE_E4,       0, NOTE_G4,
+                0,             0, NOTE_D4,       0,
+                NOTE_E4,       0, NOTE_G4,       0,
+                NOTE_A4,       0, NOTE_C5,       0};
+/*how deep are we in the melody array*/
+unsigned int melody_pos = 0;
 
 void setup() {
-  /*Setup hardware inputs/outputs. These pins are defined in the hardware_versions header file*/
+  /*Setup hardware inputs/outputs. These pins are defined in the
+    hardware_versions header file*/
   Serial.begin(9600);
   
   //Enable pull ups on inputs
@@ -29,12 +52,10 @@ void setup() {
 
   pinMode(BUZZER1,       OUTPUT);
   pinMode(BUZZER2,       OUTPUT);
-  digitalWrite(BUZZER1, LOW); // setup the "BUZZER1" side of the buzzer to stay low, while we play the tone on the other pin.
-  
-  //setLEDs(CHOICE_RED | CHOICE_GREEN | CHOICE_BLUE | CHOICE_YELLOW); // Turn all LEDs on
-  //delay(1000);
-  //setLEDs(CHOICE_NONE); // Turn off LEDs
-  Serial.write("\nStarting\r\n");
+  // setup the "BUZZER1" side of the buzzer to stay low, while we play the tone on the other pin.
+  digitalWrite(BUZZER1, LOW); 
+
+  attractMode(); //make the think blink like crazy
 }
 
 byte checkButton(void) {
@@ -60,39 +81,53 @@ void setLEDs(byte leds) {
   else                             digitalWrite(LED_YELLOW,  LOW);
 }
 
-void attractMode(void) {
+byte attractMode(void) {
   /* Show an "attract mode" display while waiting for user to press button.*/
-  setLEDs(CHOICE_RED | CHOICE_GREEN | CHOICE_BLUE | CHOICE_YELLOW); // Turn all LEDs on
-  while(1) if(checkButton() != CHOICE_NONE) break;
-  setLEDs(CHOICE_NONE);
+  unsigned long time;
+  bool ok = false;
+  byte button;
+  Serial.write("Waiting for small children to play with me\r\n");
+  while(!ok) {
+    //loop for BLINK_EVERY ms lookin for a keypress with LEDs ON
+    setLEDs(CHOICE_RED | CHOICE_GREEN | CHOICE_BLUE | CHOICE_YELLOW); // Turn all LEDs on
+    //send a tone of the melody
+    tone(BUZZER2, melody[melody_pos++], BLINK_EVERY+10); melody_pos %= sizeof(melody); //force melody_pos to [0: sizeof(melody)), where sizeof(melody) is always positive
+    time = millis();
+    //this while polls for a checkButton, set the value to button, and repeats for BLINK_EVERY ms
+    while( ( (button = checkButton()) == CHOICE_NONE) && (millis() - time < BLINK_EVERY)) ;
+    if (button != CHOICE_NONE) return button; //signal to bail out of 
+    setLEDs(CHOICE_NONE);
+    //send another tone of the melody
+    tone(BUZZER2, melody[melody_pos++], BLINK_EVERY+10); melody_pos %= sizeof(melody);
+    time = millis();
+    while( ( (button = checkButton()) == CHOICE_NONE) && (millis() - time < BLINK_EVERY)) ;
+    if (button != CHOICE_NONE) return button;
+  }
 }
-
+byte getAKeyPress() {
+  /*Loop until we get a keypress*/
+  byte key;
+  unsigned long time=millis(), offtime=millis();
+  while((key = checkButton()) == CHOICE_NONE) {
+    if (millis() - time > TONE_LENGTH)  setLEDs(CHOICE_NONE);
+    if (millis() - offtime > TIME_TILL_BLINKY)      return attractMode();
+  }
+}
 void loop() {
   /*Go baby go!!*/
-  byte key;
-  unsigned long time=0;
-  attractMode();
-  while(1) { //loop forever
-    while( (key = checkButton()) == CHOICE_NONE) {
-      if ((time != 0) && (millis() - time > TONE_LENGTH) ) {
-        time = 0;
-        setLEDs(CHOICE_NONE);
-      }
-    }
-    setLEDs(key);
-    /*
-      Light an LED and play tone
-      Red, upper left:     440Hz - 2.272ms - 1.136ms pulse
-      Green, upper right:  880Hz - 1.136ms - 0.568ms pulse
-      Blue, lower left:    587.33Hz - 1.702ms - 0.851ms pulse
-      Yellow, lower right: 784Hz - 1.276ms - 0.638ms pulse
-    */
-    time = millis();
-    switch(key) {
-    case CHOICE_RED:    tone(BUZZER2, NOTE_A4, TONE_LENGTH); break;
-    case CHOICE_GREEN:  tone(BUZZER2, NOTE_A5, TONE_LENGTH); break;
-    case CHOICE_BLUE:   tone(BUZZER2, NOTE_D5, TONE_LENGTH); break;
-    case CHOICE_YELLOW: tone(BUZZER2, NOTE_G5, TONE_LENGTH); break;
-    }
+  byte key = getAKeyPress();
+  setLEDs(key);
+  /*
+    Light an LED and play tone
+    Red, upper left:     NOTE_A4 = 440Hz    - 2.272ms - 1.136ms pulse
+    Green, upper right:  NOTE_A5 = 880Hz    - 1.136ms - 0.568ms pulse
+    Blue, lower left:    NOTE_D5 = 587.33Hz - 1.702ms - 0.851ms pulse
+    Yellow, lower right: NOTE_G5 = 784Hz    - 1.276ms - 0.638ms pulse
+  */
+  switch(key) {
+  case CHOICE_RED:    tone(BUZZER2, NOTE_A4, TONE_LENGTH); break;
+  case CHOICE_GREEN:  tone(BUZZER2, NOTE_A5, TONE_LENGTH); break;
+  case CHOICE_BLUE:   tone(BUZZER2, NOTE_D5, TONE_LENGTH); break;
+  case CHOICE_YELLOW: tone(BUZZER2, NOTE_G5, TONE_LENGTH); break;
   }
 }
